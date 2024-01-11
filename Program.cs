7ce31a20e4fa0303c83fb722e358b362bc028e96
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-11 17:13:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HTTPCookie
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Fred";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
